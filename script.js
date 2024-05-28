b4ed8b0e3dfaba41152c3e09dcab82273919dedb
JHENYFER VICTORIA NEVES DOS SANTOS <55406272888@info.local>
2024-05-28 09:40:57 -0300

    feat: menu e formulario concluidos

---
+++ script.js
@@ -0,0 +1,93 @@
+let menu = document.getElementById("menu")
+let iconeBarras = document.getElementById("icone-barras")
+let iconeX = document.getElementById("icone-x")
+
+function abreFechaMenu() {
+    // Menu fechado - tem a classe menu-fechado
+    // Menu aberto - não tem a classe menu-fechado
+
+    // Alterna a classe menu-fechado
+    // menu.classList.toggle("menu-fechado")
+
+    if (menu.classList.contains("menu-fechado")) {
+        // Abrir o menu - remover a classe menu-fechado
+        menu.classList.remove("menu-fechado")
+
+        // Esconder icone barras
+        iconeBarras.style.display = "none"
+
+        // Mostrar o icone do X
+        iconeX.style.display = "inline"
+
+    }
+
+    else {
+        // Fechar o menu - adicionar a classe menu-fechado
+        menu.classList.add("menu-fechado")
+
+        // Esconder icone do X
+        iconeX.style.display = "none"
+
+        // Mostrar o icone barras
+        iconeBarras.style.display = "inline"
+    }
+
+}
+
+onresize = () => {
+    // Abrir o menu - remover a classe menu-fechado
+    menu.classList.remove("menu-fechado")
+
+    // Esconder icone barras
+    iconeBarras.style.display = "none"
+
+    // Mostrar o icone do X
+    iconeX.style.display = "inline"
+}
+
+
+function solicitarOrcamento(event) {
+    // Pegar os valores dos imputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-texto").value
+
+    // Organizar os valores em um objeto
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+
+
+    // Enviar a requisição pra a API
+    // Metodo HTTP POST - Create/Criar -> Cadastrar um novo registro (solicitacao)
+    fetch("http://localhost:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+        // CASO SUCESSO
+        .then(resposta => {
+            console.log(resposta)
+
+            // LImpar os inputs
+            document.querySelector("#contato form").reset()
+
+            // Mostrar um alert de sucesso
+            alert("Solicitação enviada com sucesso!!! ")
+        })
+
+        // CASO ERRO
+        .catch(erro => {
+            console.log(erro)
+
+            // Mostrar alert com mensagem de erro
+            alert("Erro na requisição")
+        })
+
+        event.preventDefault()
+}
